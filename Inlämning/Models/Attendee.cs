30dcc81ae5eb4f64f1be8164787052a1620e46fc
Martin <martin.knowles@hotmail.se>
2021-03-25 18:36:44 +0100

    first push

---
+++ Inlämning/Models/Attendee.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Inlämning.Models
+{
+    public class Attendee
+    {
+        public int AttendeeID { get; set; }
+        public string Name { get; set; }
+        public string emailaddress { get; set; }
+        public int phoneNumber { get; set; }
+
+    }
+}
